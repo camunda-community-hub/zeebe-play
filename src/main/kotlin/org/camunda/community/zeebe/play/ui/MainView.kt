4f7bb0b226ea3a59caa147a3392b34916ccc26a3
Philipp Ossler <philipp.ossler@gmail.com>
2022-03-23 09:56:39 +0100

    fix: redirect root request

--- src/main/kotlin/org/camunda/community/zeebe/play/ui/MainView.kt
+++ src/main/kotlin/org/camunda/community/zeebe/play/ui/MainView.kt
@@ -3,13 +3,15 @@ package org.camunda.community.zeebe.play.ui
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.view.RedirectView
 
 @Component
+@RequestMapping
 class MainView {
 
     @GetMapping("/")
-    fun index(): String {
-        return home()
+    fun index(): RedirectView {
+        return RedirectView("/view/home")
     }
 
     @GetMapping("/view/home")
