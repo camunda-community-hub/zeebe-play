5b51fc450be147d208ac78e180cfddd69043a08b
Philipp Ossler <philipp.ossler@gmail.com>
2023-02-01 06:05:35 +0100

    feat: add REST API for connector secrets

---
+++ src/main/kotlin/org/camunda/community/zeebe/play/rest/ConnectorSecretsResource.kt
@@ -0,0 +1,53 @@
+package org.camunda.community.zeebe.play.rest
+
+import org.camunda.community.zeebe.play.connectors.ConnectorSecret
+import org.camunda.community.zeebe.play.connectors.ConnectorSecretRepository
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/rest/connector-secrets")
+class ConnectorSecretsResource(
+    private val repository: ConnectorSecretRepository
+) {
+
+    @RequestMapping(path = ["/"], method = [RequestMethod.GET])
+    fun getSecrets(): ConnectorSecretsDto {
+        return repository.findAll()
+            .map {
+                ConnectSecretDto(
+                    name = it.name,
+                    value = it.value
+                )
+            }.let {
+                ConnectorSecretsDto(
+                    secrets = it
+                )
+            }
+    }
+
+    @RequestMapping(path = ["/"], method = [RequestMethod.POST])
+    fun setSecrets(@RequestBody dto: ConnectorSecretsDto) {
+        dto.secrets
+            .map {
+                ConnectorSecret(
+                    name = it.name,
+                    value = it.value
+                )
+            }
+            .let {
+                repository.saveAll(it)
+            }
+    }
+
+    data class ConnectorSecretsDto(
+        val secrets: List<ConnectSecretDto>
+    )
+
+    data class ConnectSecretDto(
+        val name: String,
+        val value: String
+    )
+}
